a64b31d17f32e4a005259ed28e6395e8791caeed
henryleecode23 <99625462+henryleecode23@users.noreply.github.com>
2022-03-31 21:48:10 +0800

    fix utf-8

--- a003/main.cpp
+++ a003/main.cpp
@@ -1,5 +1,3 @@
-//https://zerojudge.tw/ShowProblem?problemid=a003
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,12 +6,12 @@ int main(int argc, char** argv) {
 	int M,D;
 	cin>>M>>D;
 	if ((M*2+D)%3 == 0){
-		cout<<"´¶";
+		cout<<"普通";
 	}
 	else if ((M*2+D)%3 == 1){
-		cout<<"¦N";
+		cout<<"吉";
 	}
 	else{
-		cout<<"¤j¦N";
+		cout<<"大吉";
 	}
 }
