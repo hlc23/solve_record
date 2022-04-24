44256586b0e9797ccfb4b54784910d9228c4dcfa
leeuho16 <henry.lee.code23@gmail.com>
2022-04-24 10:32:32 +0800

    add extra infomation to writer

--- md_writer.py
+++ md_writer.py
@@ -127,6 +127,7 @@ with open("README.md",mode = "w",encoding="utf-8"):
     pass
 
 with open("README.md", mode="w",encoding="utf-8") as readme:
+    # zerojudge
     readme.write("# 解題紀錄\n\n## zerojudge\n\n### 題目\n")
     if len(cpp_list) > 0:
         readme.write("\n#### cpp\n\n")
@@ -147,5 +148,8 @@ with open("README.md", mode="w",encoding="utf-8") as readme:
         readme.write(f'''### {n}\n\n''')
         for p in sorted(os.listdir(f"./NHDK/{n}")):
             readme.write(f'''[{p}](./NHDK/{n}/{p}/)\n\n''')
+    # extra
+    with open("extra.md",mode="r",encoding="utf-8") as extra:
+        readme.write(extra.read())
     readme.write('''\n## 作者的話:\n\n這個網站是使用Github page及自己寫的腳本和爬蟲所完成，目的是想記錄自己解題~~也許還能順便當學習歷程~~，我認為在許多細節還有許多地方可以改進，以及程式能有更好的寫法，如果有什麼建議想要提供，或是我的程式有哪些地方有Bug想要告訴我，歡迎到這個專案的[Github頁面](https://henryleecode23.github.io/solve_record/)發issues或是透過電子郵件聯絡我\n\nGmail: henry.lee.code23@gmail.com''')
     readme.write(f'''\n\n\n\n最後更新時間: {get_ymd()} {get_time_with_word()}\n''')
--- zerojudge/e835/README.md
+++ zerojudge/e835/README.md
@@ -84,4 +84,4 @@ cpp
 
 ## [回首頁](https://henryleecode23.github.io/solve_record/)
 
-此頁面最後編輯時間: 2022.4.23 15點57分
+此頁面最後編輯時間: 2022.4.24 10點31分
