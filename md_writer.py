ac1f432c00fde6045634e9caff089714b02d4b4a
leeuho16 <henry.lee.code23@gmail.com>
2022-04-24 12:32:21 +0800

    add tip for time

--- md_writer.py
+++ md_writer.py
@@ -153,3 +153,4 @@ with open("README.md", mode="w",encoding="utf-8") as readme:
         readme.write(extra.read())
     readme.write('''\n## 作者的話:\n\n這個網站是使用Github page及自己寫的腳本和爬蟲所完成，目的是想記錄自己解題~~也許還能順便當學習歷程~~，我認為在許多細節還有許多地方可以改進，以及程式能有更好的寫法，如果有什麼建議想要提供，或是我的程式有哪些地方有Bug想要告訴我，歡迎到這個專案的[Github頁面](https://henryleecode23.github.io/solve_record/)發issues或是透過電子郵件聯絡我\n\nGmail: henry.lee.code23@gmail.com''')
     readme.write(f'''\n\n\n\n最後更新時間: {get_ymd()} {get_time_with_word()}\n''')
+    readme.write('''最後更新時間可能因程式運行裝置不同而有所變化''')
