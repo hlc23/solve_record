bb2b4a043981156db51f1b396ce70673c5f3a488
henryleecode23 <99625462+henryleecode23@users.noreply.github.com>
2023-03-14 01:05:16 +0800

    fix: c013

--- zerojudge/c013/main.cpp
+++ zerojudge/c013/main.cpp
@@ -4,25 +4,20 @@ using namespace std;
 int main() {
     int test_case, A,F;
     cin>>test_case;
-    for (int i = 0; i<=test_case; i++){
-        // 此測資有test_case組數據
+    for (int i = 0; i<test_case; i++){
         cin>>A>>F;
         for (int time = 0; time < F; time++){
             for (int n = 1;n<=A; n++){
-                for (int t = 1; t<=n;t++){
-                    cout<<n;
-                }
+                for (int t = 1; t<=n;t++) cout<<n;
                 cout<<endl;
             }
             for (int n = A-1; n>=1;n--){
-                for (int t = 1; t<=n; t++){
-                    cout<<n;
-                }
+                for (int t = 1; t<=n; t++) cout<<n;
                 cout<<endl;
             }
             cout<<endl;
         }
-        A = F = 0;
+        // A=0; F=0;
     }
     return 0;
 }
